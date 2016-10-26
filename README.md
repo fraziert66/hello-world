778ec088bd62189894fb260b2dcb74a1e73ddddf
Thomas Frazier <tfrazier@PC04.cis.com>
2016-10-26 10:13:14 -0500

    added hello.py

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # hello-world
+just a test
+
+Changelog
+added helloworld.py - 10/26/16
---
+++ hello.py
@@ -0,0 +1,6 @@
+#helloworld.py
+#Thomas Frazier
+#10/26/16
+'''Tells the world hello'''
+
+print('Hello World')
